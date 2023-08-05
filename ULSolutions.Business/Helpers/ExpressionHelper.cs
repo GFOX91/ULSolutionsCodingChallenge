2deba1f2fef4d1097f0d03da0488751d642fc25a
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-08-05 11:00:12 +0100

    creation of expression helper, with evaluate method containing breif plan

---
+++ ULSolutions.Business/Helpers/ExpressionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULSolutions.Business.Helpers
+{
+    internal class ExpressionHelper
+    {
+    }
+}
