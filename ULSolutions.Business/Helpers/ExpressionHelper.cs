6716a41d68eaab7f083b4ef4be803ea1967edefc
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-08-05 11:37:18 +0100

    Added line of code to remove spaces from expressions, so those expressions provided with spaces between the numbers and operators can be conmverted to the expected format and still processed

--- ULSolutions.Business/Helpers/ExpressionHelper.cs
+++ ULSolutions.Business/Helpers/ExpressionHelper.cs
@@ -14,10 +14,11 @@ namespace ULSolutions.Business.Helpers
             // 1. Perform some bounds checking - Validate string provided is not null and is in expected form, return relevant exceptions if any checking fails
             if (string.IsNullOrWhiteSpace(expression)) throw new ArgumentException("expression cannot be null or empty");
 
-            Regex expectedFormat = new Regex(@"^([0-9]+[-+*\/])+[0-9]+$");
-            if (expectedFormat.IsMatch(expression) == false) throw new ArgumentException("Expression is in inavlid format");
+            expression = String.Concat(expression.Where(c => !Char.IsWhiteSpace(c)));
 
-            // 2. Remove any spaces in the string as calculation will be performed by iterating through chars
+            Regex expectedFormat = new Regex(@"^([0-9]+[-+*\/])+[0-9]+$");
+            if (expectedFormat.IsMatch(expression) == false) throw new ArgumentException("Expression is in invalid format");
+
             // 3. Find the value of the first number in the expression, will be used as initial sum in calculation
             // 4. Iterate through remaining characters in the expression and update the existing sum value based on the next operator and number
             // 5. return the final sum
