dc132c02f8bb85bb4f182153fa152079b22f00d0
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-08-05 11:17:39 +0100

    Got tests passing

--- ULSolutions.Business/Helpers/ExpressionHelper.cs
+++ ULSolutions.Business/Helpers/ExpressionHelper.cs
@@ -11,6 +11,10 @@ namespace ULSolutions.Business.Helpers
         public double Evaluate(string expression)
         {
             // 1. Perform some bounds checking - Validate string provided is not null and is in expected form, return relevant exceptions if any checking fails
+            if (string.IsNullOrWhiteSpace(expression)) throw new ArgumentException();
+
+
+
             // 2. Remove any spaces in the string as calculation will be performed by iterating through chars
             // 3. Find the value of the first number in the expression, will be used as initial sum in calculation
             // 4. Iterate through remaining characters in the expression and update the existing sum value based on the next operator and number
