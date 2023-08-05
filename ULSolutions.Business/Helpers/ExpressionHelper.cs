f7ee27ad54c94ecc24f588242213d7239a321e32
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-08-05 11:15:30 +0100

    Wrote failing test for checking if expression is null

--- ULSolutions.Business/Helpers/ExpressionHelper.cs
+++ ULSolutions.Business/Helpers/ExpressionHelper.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ULSolutions.Business.Helpers
 {
-    internal class ExpressionHelper
+    public class ExpressionHelper
     {
+        public double Evaluate(string expression)
+        {
+            // 1. Perform some bounds checking - Validate string provided is not null and is in expected form, return relevant exceptions if any checking fails
+            // 2. Remove any spaces in the string as calculation will be performed by iterating through chars
+            // 3. Find the value of the first number in the expression, will be used as initial sum in calculation
+            // 4. Iterate through remaining characters in the expression and update the existing sum value based on the next operator and number
+            // 5. return the final sum
+
+            return 0;
+        }
     }
 }
---
+++ ULSolutions.Test/Business/ExpressionTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULSolutions.Business.Helpers;
+
+namespace ULSolutions.Test.Business
+{
+    public class ExpressionTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Evaluate_ThrowsException_WhenExpressionIsNullOrWhiteSpace(string expression)
+        {
+            // Arrange
+            var sut = new ExpressionHelper();
+
+            // Act
+            var result = () => sut.Evaluate(expression);
+
+            // Assert
+            result.Should().Throw<ArgumentException>().WithMessage("expression cannot be null or empty");
+        }
+    }
+}
